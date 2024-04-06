f748031c8f71c56426424cf60f1c5c92924ad84b
maximilien <mvidiani@enseirb.fr>
2024-04-06 19:19:28 +0200

    join

--- src/thread_join.c
+++ src/thread_join.c
@@ -1,5 +1,18 @@
 #include "global.h"
 
 int thread_join(thread_t thread, void **retval){
+    struct_thread_t *thread_to_join = id_to_struct(thread);
+    if (thread_to_join == NULL) {
+        return -1;
+    }
+
+    while (!thread_to_join->finished) {
+        thread_yield();
+    }
+
+    if (retval != NULL) {
+        *retval = thread_to_join->retval;
+    }
+
     return 0;
 }
