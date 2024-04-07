5bbb8d6136ddef156609cf492ddb8c6151826d64
maximilien <mvidiani@enseirb.fr>
2024-04-07 18:52:36 +0200

    exit

--- src/thread_exit.c
+++ src/thread_exit.c
@@ -4,10 +4,36 @@
 
 #include "global.h"
 
+
+int thread_cancel(thread_t thread) {
+    struct_thread_t *thread_to_cancel = search_by_id(&ready_threads, thread);
+
+    if (thread_to_cancel == NULL) {
+        return -1;
+    }
+
+    else {
+        remove_thread(&ready_threads, thread_to_cancel);
+        free(thread_to_cancel->context.uc_stack.ss_sp);
+        free(thread_to_cancel);
+    }
+
+    if (thread_to_cancel == thread_self()) {
+        cancel_current = 1;
+        scheduler();
+        return 0;
+    }
+
+    return 0;
+}
+
+
 void thread_exit(void *retval) {
 
-    current_thread->retval = retval;    
-    current_thread->finished = 1;
-    thread_join(current_thread->id, NULL);
+    current_thread->retval = retval;
+
+    enqueue(&finished_threads, current_thread);
+    remove_thread(&ready_threads, current_thread);
+    thread_cancel(thread_self());
 
 }
