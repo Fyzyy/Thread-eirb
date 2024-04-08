8d4f3d2f22ecaee5375578e8f9d719c7a2c59f36
Ilyes Bechoual <ilyes.bechoual@bordeaux-inp.fr>
2024-04-08 09:20:59 +0200

    fix thread_mutex

--- src/thread_mutex.c
+++ src/thread_mutex.c
@@ -1,5 +1,6 @@
 #include "global.h"
 #include <assert.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -22,11 +23,13 @@ int thread_mutex_destroy(thread_mutex_t *mutex) {
 
 int thread_mutex_lock(thread_mutex_t *mutex) {
   while (__sync_lock_test_and_set(&(mutex->dummy), 1)) {
-    ucontext_t current_context;
-    getcontext(&current_context);
-
-    makecontext(&(current_context), (void (*)(void))thread_mutex_lock, 1,
-                mutex);
+    // ucontext_t current_context;
+    // getcontext(&current_context);
+    //
+    // makecontext(&(current_context), (void (*)(void))thread_mutex_lock, 1,
+    // mutex,
+    //             NULL);
+    sched_yield();
   }
   // printf("locked\n");
   return 0;
