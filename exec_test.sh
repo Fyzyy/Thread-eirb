017d5066e41410589b689b63b827e86d93e1671b
Robin ROUET <rrouet@enseirb-matmeca.fr>
2024-04-29 09:38:52 +0200

    Same for make check

--- exec_test.sh
+++ exec_test.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 repo="./build"
-NB_THREADS_TO_CREATE=10
-NB_THREADS_TO_YIELD=10
+NB_THREADS_TO_CREATE=20
+NB_THREADS_TO_YIELD=20
 
 for file in "$repo"/*
 do
-    numb=$(basename "$file" | cut -c 1-2)
+    numb=$(basename "$file" | cut -c 1-2);
     if [ $(($numb)) -gt 12 ]; then
-        if [ $(($numb)) -eq 31 ] || [ $(($numb)) -eq 32 ] || [ $(($numb)) -eq 33 ]; then
-            echo $file $NB_THREADS_TO_CREATE $NB_THREADS_TO_YIELD;
-            $file $NB_THREADS_TO_CREATE $NB_THREADS_TO_YIELD;
+        if [ $(($numb)) -eq 31 ] || [ $(($numb)) -eq 32 ]; then
+            echo $file 10 $NB_THREADS_TO_YIELD;
+            $file 10 $NB_THREADS_TO_YIELD; \
+        elif [ $((numb)) -eq 33 ]; then
+            echo $file $NB_THREADS_TO_CREATE 5;
+            $file $NB_THREADS_TO_CREATE $NB_THREADS_TO_YIELD; \
+        elif [ $((numb)) -eq 51 ]; then
+            echo $file 8;
+            $file 8; \
+        elif [ $((numb)) -eq 71 ]; then
+            echo $file 5;
+            $file 5; \
         else
             echo $file $NB_THREADS_TO_CREATE;
-            $file $NB_THREADS_TO_CREATE;
+            $file $NB_THREADS_TO_CREATE; \
         fi
     else 
         echo $file;
-        $file;
+        $file; \
     fi
     echo "";
+    echo "******************************************************************************";
 done
