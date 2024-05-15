1629686730fbf92ca09507da4222b7001e46ab25
Ilyes Bechoual <ilyes.bechoual@bordeaux-inp.fr>
2024-05-15 19:59:28 +0200

    fixing deadlock

--- src/thread_join.c
+++ src/thread_join.c
@@ -1,9 +1,10 @@
 #include "global.h"
+#include "thread.h"
 #include <errno.h>
 #include <stdio.h>
 
 #include <time.h>
-#define TIMEOUT_SECONDS 5
+#define TIMEOUT_SECONDS 3
 
 int thread_join(thread_t thread, void **retval) {
 
@@ -13,9 +14,12 @@ int thread_join(thread_t thread, void **retval) {
       if (is_empty(&finished_threads)) {
         cancel_current = 1;
         dead = 1;
+        thread_yield();
       }
+    } else {
+
+      thread_yield();
     }
-    thread_yield();
   }
 
   struct_thread_t *thread_to_join = search_by_id(&finished_threads, thread);
@@ -26,6 +30,10 @@ int thread_join(thread_t thread, void **retval) {
       *retval = thread_to_join->retval;
 
     free_thread(thread_to_join);
+    if (size(&ready_threads) > 0) {
+      struct_thread_t *thread_to_free = dequeue(&ready_threads);
+      free_thread(thread_to_free);
+    }
 
     if (dead) {
       dead = 0;
@@ -33,7 +41,10 @@ int thread_join(thread_t thread, void **retval) {
     }
     return 0;
   }
-
+  if (size(&ready_threads) > 0) {
+    struct_thread_t *thread_to_free = dequeue(&ready_threads);
+    free_thread(thread_to_free);
+  }
   if (dead) {
     dead = 0;
     return EDEADLK;
